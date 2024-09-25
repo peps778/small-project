ef9f7ebdb73e4f86b68fe61de37b35805cf56292
polks <magbanuatx56@gmail.com>
2024-09-25 10:34:27 -0400

    major changes

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -40,13 +40,13 @@ type TConfig = {
 
 export const config: TConfig = {
   html: {
-    title: "John Doe — 3D Portfolio",
-    fullName: "John Doe",
-    email: "johndoe@mail.com",
+    title: "Paul Jhon Magbanua - Porfolio",
+    fullName: "Paul Jhon Magbanua",
+    email: "magbanuatx56@mail.com",
   },
   hero: {
-    name: "John Doe",
-    p: ["I develop 3D visuals, user", "interfaces and web applications"],
+    name: "Paul Jhon",
+    p: ["I develop desktop apps, user", "interfaces and web applications"],
   },
   contact: {
     p: "Get in touch",
