3bf3b38e099bb74a70cfa80ebbf82229345b66b8
polks <magbanuatx56@gmail.com>
2024-10-18 13:53:03 -0400

    commute

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// vite config <3
 export default defineConfig({
   plugins: [react()],
   base: "/small-project/",
