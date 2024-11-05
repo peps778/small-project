54d360a8878be1d6e75ac7678455c716dbf391e9
polks <magbanuatx56@gmail.com>
2024-11-05 17:06:21 -0500

    fdsdfsdf

--- src/components/index.ts
+++ src/components/index.ts
@@ -14,7 +14,7 @@ import Works from "./sections/Works";
 import Feedbacks from "./sections/Feedbacks";
 import Contact from "./sections/Contact";
 import CanvasLoader from "./layout/Loader";
-import spsImage from "./assets/sps.png";
+import spsImage from "./sps.png";
 
 export {
   Hero,
