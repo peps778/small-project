e8237d7903e1e099fcb6e6d38d5ce5bb191eb92e
polks <magbanuatx56@gmail.com>
2024-11-06 17:16:13 -0500

    vite config

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,23 @@
 /// <reference types="vite/client" />
+
+// Declare modules for image file types
+declare module "*.png" {
+    const value: string;
+    export default value;
+  }
+  
+  declare module "*.jpg" {
+    const value: string;
+    export default value;
+  }
+  
+  declare module "*.jpeg" {
+    const value: string;
+    export default value;
+  }
+  
+  declare module "*.gif" {
+    const value: string;
+    export default value;
+  }
+  
