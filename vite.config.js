3752989559e397a2d29d6631f83b932839f20c2f
polks <magbanuatx56@gmail.com>
2024-11-07 16:37:36 -0500

    shesh guba

--- vite.config.js
+++ vite.config.js
@@ -6,18 +6,6 @@ export default defineConfig({
   base: "/small-project/", // Adjust based on your deployment setup
   chunkSizeWarningLimit: 2000,
   build: {
-    outDir: 'dist', // Ensure output is directed to 'dist' folder
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          // Create custom chunks based on modules
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) return 'react'; // Separate React-related libraries
-            if (id.includes('lodash')) return 'lodash'; // Separate lodash if used
-            return 'vendor'; // Group remaining libraries
-          }
-        },
-      },
-    },
+    outDir: 'dist', 
   },
 });
